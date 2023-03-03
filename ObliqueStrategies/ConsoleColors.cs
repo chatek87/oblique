e09bf3b6e6f1d40a39ec393d9e22878cbbc115e5
Charlie Tekulve <charlietekulve@gmail.com>
2023-03-03 14:57:45 -0500

    initial commit

---
+++ ObliqueStrategies/ConsoleColors.cs
@@ -0,0 +1,22 @@
+namespace ObliqueStrategies;
+
+internal static class ColorChooser
+{
+    public static ConsoleColor[] Colors =
+    {
+        ConsoleColor.Red,
+        ConsoleColor.DarkRed,
+        ConsoleColor.Green,
+        ConsoleColor.DarkGreen,
+        ConsoleColor.Blue,
+        ConsoleColor.DarkBlue,
+        ConsoleColor.Magenta,
+        ConsoleColor.DarkMagenta,
+        ConsoleColor.Yellow,
+        ConsoleColor.DarkYellow,
+        ConsoleColor.Cyan,
+        ConsoleColor.DarkCyan,
+        ConsoleColor.White,
+        ConsoleColor.Gray
+    };
+}
---
+++ ObliqueStrategies/Program.cs
@@ -0,0 +1,16 @@
+﻿using ObliqueStrategies;
+
+Random randomColor = new Random();
+int colorIndex = randomColor.Next(ColorChooser.Colors.Length - 1);
+
+Console.ForegroundColor = ColorChooser.Colors[colorIndex];
+
+
+Random randomCard = new Random();
+int cardIndex = randomCard.Next(ObliqueStrategiesDeck.Cards.Length - 1);
+
+Console.WriteLine(ObliqueStrategiesDeck.Cards[cardIndex]);
+
+
+
+Console.ReadKey();
